1f2b8f9c4c5352141dda3dae6e107f4c9cd204c7
titenq <titenq@gmail.com>
2021-06-28 23:22:15 -0300

    revalidate in pages

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -38,6 +38,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   }
 
   return {
+    revalidate: 60,
     props: {
       heading: page.heading,
       body: page.body.html
